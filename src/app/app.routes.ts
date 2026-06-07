24b8589c7384ef1184828d2671bb65aa5210d6b1
Eduardo-Henderson <eduardo.henderson@estudiantes.utec.edu.uy>
2026-06-07 11:08:25 -0300

    registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './views/auth/login/login';
 import { RegistroEspacioComponent } from './views/espacios/registro-espacio/registro-espacio';
 import { ReservasComponent } from './views/reservas/reservas';
 import { Disponibilidad } from './views/disponibilidad/disponibilidad';
+import { RegistroComponent } from './views/registroUsuario/registro';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'espacios', component: RegistroEspacioComponent },
   { path: 'registro-espacio', component: RegistroEspacioComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: 'reservas', component: ReservasComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'disponibilidad', component: Disponibilidad },
---
+++ src/app/views/registroUsuario/registro.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './registro.html',
+  styleUrls: ['./registro.css']
+})
+export class RegistroComponent {
+  usuario = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: '',
+    fechaNacimiento: '',
+    cedula: '',
+    tipousuario: 'USUARIO', 
+    estadoUsuario: 'ACTIVO',
+    contrasenia: '' 
+  };
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onRegister() {
+    console.log('Datos del usuario a registrar:', this.usuario); // 🔥 DEBUG
+    // Aquí disparamos la petición al backend al presionar el botón
+    this.authService.registro(this.usuario).subscribe({
+      next: (response) => {
+        alert('¡Usuario registrado con éxito!');
+        this.router.navigate(['/login']); 
+      },
+      error: (error) => {
+        console.error(error);
+        alert('Hubo un error en el registro');
+      }
+    });
+  }
+}
