8e574fd8d2c4e238e0c8cdfd634908e00ce79ff0
Maximiliano Moreira <maximiliano.moreira@estudiantes.utec.edu.uy>
2026-06-21 13:45:46 -0300

    fix

--- src/app/views/layout/layout.ts
+++ src/app/views/layout/layout.ts
@@ -27,7 +27,12 @@ export class LayoutComponent {
   itemsMenu: ItemMenu[] = [
     { texto: 'Espacios', ruta: '/espacios', icono: 'bi bi-calendar2-check' },
     { texto: 'Tus reservas', ruta: '/reservas', icono: 'bi bi-bookmark-star', soloLogueado: true },
-    { texto: 'Control Reservas', ruta: '/reservas-globales', icono: 'bi bi-clipboard-data'/*,soloAdmin: true*/},
+    {
+      texto: 'Control Reservas',
+      ruta: '/reservas-globales',
+      icono: 'bi bi-clipboard-data',
+      soloAdmin: true,
+    },
     {
       texto: 'Disponibilidad',
       ruta: '/disponibilidad',
